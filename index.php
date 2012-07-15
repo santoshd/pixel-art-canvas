f231aacbfaa7f20714e72c4b5141920fcb2ab888
Meher Ranjan H <hmeherranjan@gmail.com>
2012-07-15 19:13:50 +0530

    added author template.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<!--
+  Author: Meher Ranjan[ @meherranjan ]
+  Description: Pixel Art Canvas
+-->
 <!doctype html>
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
